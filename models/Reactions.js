eb8cad153682a034bc4d510bc87213995508f8bb
Emily Pozzi <epozzi5@yahoo.com>
2021-06-24 19:22:02 -0500

    JS

--- models/Reactions.js
+++ models/Reactions.js
@@ -1,2 +1,8 @@
 const { Schema, model, Types } = require('mongoose');
 const dateFormat = require('../utils/dateFormat')
+
+const reactionSchema = new Schema({
+
+});
+
+module.exports = reactionSchema;
--- models/Thought.js
+++ models/Thought.js
@@ -1,2 +1,9 @@
 const { Schema, model } = require('mongoose')
 const dateFormat = require('../utils/dateFormat')
+const reactSchema = require('./Reactions')
+
+const thoughtSchema = new Schema(
+
+)
+
+module.exports = Thought;
