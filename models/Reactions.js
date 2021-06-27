d758f3fee912955f92c5be9d63658dea827f1cc9
Emily Pozzi <epozzi5@yahoo.com>
2021-06-27 18:43:45 -0500

    js

--- models/Reactions.js
+++ models/Reactions.js
@@ -2,6 +2,19 @@ const { Schema, model, Types } = require('mongoose');
 const dateFormat = require('../utils/dateFormat')
 
 const reactionSchema = new Schema({
+    usrname: {
+        type: String,
+        required: true
+    },
+    reactId: {
+        type: Types.ObjectId,
+        default: new Types.ObjectId()
+    },
+    reactBody: {
+        type: String,
+        required: true,
+        maxLength: 350
+    },
 
 });
 
--- models/Thought.js
+++ models/Thought.js
@@ -3,7 +3,23 @@ const dateFormat = require('../utils/dateFormat')
 const reactSchema = require('./Reactions')
 
 const thoughtSchema = new Schema(
-
+{
+    usrname: {
+        type: String,
+        required: true
+    },
+    thoughtInfo: {
+        type: String,
+        required: true,
+        min: 5,
+        max: 350
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        get: (createdAtVal) => dateFormat(createdAtVal)
+    },
+}
 )
 
 module.exports = Thought;
